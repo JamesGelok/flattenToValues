21bd896d1539b653d5d43079689e96abe031d841
James Gelok <jdgelok@gmail.com>
2021-02-10 13:21:12 +0000

    added exclude function

--- index.js
+++ index.js
@@ -25,23 +25,38 @@ const obj4 = [{
     six: 6,
     eight: 8,
   },3,4,5,6,7,8];
+const obj5 = {
+  0: {
+    one:   [{ myKey: 1 }, { myKey: 10 }, { myKey: 100 }],
+    three: [{ myKey: 3 }, { myKey: 30 }, { myKey: 300 }],
+    five:  [{ myKey: 5 }, { myKey: 50 }, { myKey: 500 }],
+    seven: [{ myKey: 7 }, { myKey: 70 }, { myKey: 700 }]
+  }
+}
 
-const flattenToValues = (obj) => {
+const flattenToValues = (obj, excludeFunc) => {
   return Object.keys(obj).reduce((accumulator, currentValue) => {
     const potentialValue = obj[currentValue];
+    if (excludeFunc && excludeFunc(potentialValue)) return accumulator.concat(potentialValue)
+    
     const val =
       typeof potentialValue === "object" && potentialValue !== null
-        ? flattenToValues(potentialValue)
+        ? flattenToValues(potentialValue, excludeFunc)
         : potentialValue;
     return accumulator.concat(val);
   }, []);
 };
 
 
+
 const result1 = flattenToValues(obj1);
 const result2 = flattenToValues(obj2);
 const result3 = flattenToValues(obj3);
 const result4 = flattenToValues(obj4);
+const result5 = flattenToValues(obj5, (el) => {
+  // console.log('myKey' in el)
+  return 'myKey' in el
+});
 
 console.log(obj1, 'into', result1);
 console.log("-------------------");
@@ -50,4 +65,6 @@ console.log("-------------------");
 console.log(obj3, 'into', result3);
 console.log("-------------------");
 console.log(obj4, 'into', result4);
-console.log(obj4, 'into', result4);
+console.log("-------------------");
+console.log(obj5, 'into', result5);
+
